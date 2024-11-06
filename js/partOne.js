9d3021743539936160795f9aad81fd9996e9a81e
lltosso <luis.tosso@gmail.com>
2024-11-06 13:13:53 +0100

    por terminar

---
+++ js/partOne.js
@@ -0,0 +1,11 @@
+const container = document.querySelector(".container");
+const btn = document.querySelector(".button");
+
+function changecolor(){
+    container.setAttribute("style", "background-color: darkblue;");
+    btn.addEventListener ("click", changecolor);
+
+}
+
+
+
